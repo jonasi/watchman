dd6e69978132fcb53f2cb4fb10141d676e965b26
ShawnROGrady <shawn.r.ogrady@gmail.com>
2020-02-19 14:48:01 -0600

    [client] added set log level functionality

---
+++ log_level_test.go
@@ -0,0 +1,47 @@
+package watchman
+
+import "testing"
+
+var setLogLevelTests = map[string]struct {
+	expectedLevel string
+	expectErr     bool
+}{
+	LogLevelDebug: {
+		expectedLevel: LogLevelDebug,
+		expectErr:     false,
+	},
+	LogLevelError: {
+		expectedLevel: LogLevelError,
+		expectErr:     false,
+	},
+	LogLevelOff: {
+		expectedLevel: LogLevelOff,
+		expectErr:     false,
+	},
+	"invalid_log_level": {
+		expectErr: true,
+	},
+}
+
+func TestSetLogLevel(t *testing.T) {
+	for logLevel, testCase := range setLogLevelTests {
+		t.Run(logLevel, func(t *testing.T) {
+			cl := &Client{Sockname: sock}
+			l, err := cl.SetLogLevel(logLevel)
+			if err != nil {
+				if !testCase.expectErr {
+					t.Fatalf("unexpected error setting log level: %s", err)
+				}
+				return
+			}
+
+			if testCase.expectErr {
+				t.Error("unexpectedly no error")
+			}
+
+			if l.LogLevel != testCase.expectedLevel {
+				t.Errorf("unexpected log-level (expected = '%s', actual = '%s')", testCase.expectedLevel, l.LogLevel)
+			}
+		})
+	}
+}
